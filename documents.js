f48d0f1bbb331ebc66e1cd0bf637178bc3ec2da9
Chris <ulrich@tcs-germany.de>
2018-01-03 14:11:50 +0100

    started implementation

--- documents.js
+++ documents.js
@@ -9,4 +9,48 @@ dhtmlxEvent(window,"load",function(){
 			height:55
 		}
   });
+  Documents.FillGrid = function(aGrid,aFilter,aLimit,Callback) {
+    var aURL = '/'+aDS.TableName+'/list.json';
+    if (aFilter) {
+      aURL+='?filter='+encodeURIComponent(aFilter);
+    }
+    if (aLimit) {
+      aURL+='&limit='+aLimit;
+    }
+    aDS.loading = true;
+    if (LoadData(aURL,function(aData){
+      console.log("Data loaded");
+      try {
+        aGrid.clearAll();
+        if ((aData)&&(aData.xmlDoc))
+        var aData2;
+        if (aData.xmlDoc.responseText != '')
+          aData2 = JSON.parse(aData.xmlDoc.responseText);
+        if (aData2) {
+          aData2.id = aData2.sql_id;
+            aGrid.add(aData2);
+            try {
+              aDS.DataProcessor.setUpdated(aData2[i].sql_id);
+            } catch(err) {}
+          }
+        aDS.loading = false;
+      } catch(err) {
+        aDS.loading = false;
+        console.log(aDS.TableName,'failed to load data !',err);
+      }
+      if (Callback)
+        Callback();
+    })==true) {
+      console.log("Data loading...");
+    }
+    else {
+      if (Callback)
+        Callback();
+      dhtmlx.message({
+        type : "error",
+        text: "Login erforderlich",
+        expire: 3000
+      });
+    }
+  }
 });
